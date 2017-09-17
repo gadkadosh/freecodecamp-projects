7949ab9aa195f8964a6c9f8c9eddd9ca4be3ed14
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-17 23:30:36 +0200

    random quote machine: randomize color

--- random-quote-machine/index.js
+++ random-quote-machine/index.js
@@ -9,12 +9,28 @@ const getQuote = url => {
 }
 
 const randomColor = () => {
-    rColor = []
+    bgColor = []
     for (let i = 0; i < 3; i++) {
-        rColor[i] = Math.round(Math.random() * 255)
+        bgColor[i] = Math.round(Math.random() * 255)
     }
-    rgbStr = "rgb(" + rColor[0] + ", " + rColor[1] + ", " + rColor[2] + ")"
-    $('div.jumbotron').css("background-color", rgbStr)
+
+    // console.log("bgColor:", bgColor, "total:", bgColor.reduce((acc, x) => acc + x))
+    
+    if (bgColor.reduce((acc, x) => acc + x) > 255 * 3 / 2) {
+        textColor = [20, 20, 20]
+        footerColor = [70, 70, 70]
+    } else {
+        textColor = [245, 245, 245]
+        footerColor = [200, 200, 200]
+    }
+
+    rgbBgStr = "rgb(" + bgColor[0] + ", " + bgColor[1] + ", " + bgColor[2] + ")"
+    rgbTextStr = "rgb(" + textColor[0] + ", " + textColor[1] + ", " + textColor[2] + ")"
+    rgbFooterStr = "rgb(" + footerColor[0] + ", " + footerColor[1] + ", " + footerColor[2] + ")"
+    
+    $('div.jumbotron').css("background-color", rgbBgStr)
+    $('div.jumbotron').css("color", rgbTextStr)
+    $('.blockquote-footer').css("color", rgbFooterStr)
 }
 
 $(document).ready(() => {
