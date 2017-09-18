220a1055feb4a31cfafe9c2686a37344c8b1130e
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-18 23:57:10 +0200

    Initial commit of local-weather

---
+++ local-weather/index.js
@@ -0,0 +1,12 @@
+url = "https://query.yahooapis.com/v1/public/yql"
+url += "?q=select%20*%20from%20weather.condition%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+
+$('document').ready(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+        console.log(pos)
+    })
+
+    $.getJSON(url, json => {
+        console.log(JSON.stringify(json))
+    })
+})
