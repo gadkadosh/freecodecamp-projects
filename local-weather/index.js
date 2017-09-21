471f249eb85d097d54185bae8bfc755b8e1415fd
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-21 15:19:35 +0200

    Local-weather: some progress with YQL and tranlating to query URL

--- local-weather/index.js
+++ local-weather/index.js
@@ -1,12 +1,29 @@
 url = "https://query.yahooapis.com/v1/public/yql"
-url += "?q=select%20*%20from%20weather.condition%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+query = "select * from weather.forecast where woeid = 615702"
+queryUrl = "?q=" + query.replace(/ /g, '%20').replace(/=/g, '%3D') + "&format=json"
+weatherUrl = url + queryUrl
+
+// console.log(weatherUrl)
+// console.log(url + "?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
 
 $('document').ready(() => {
-    navigator.geolocation.getCurrentPosition(pos => {
-        console.log(pos)
-    })
+    // navigator.geolocation.getCurrentPosition(pos => {
+    //     console.log("Lon:", pos.coords.longitude, "Lat", pos.coords.latitude)
+    // })
 
-    $.getJSON(url, json => {
-        console.log(JSON.stringify(json))
+    $.getJSON(weatherUrl, json => {
+        console.log('hi')
+        console.log(json.query.results.channel)
+        // console.log(JSON.stringify(json))
+
+        city = json.query.results.channel.location.city
+        country = json.query.results.channel.location.country
+        summary = json.query.results.channel.item.condition.text
+        temperature = json.query.results.channel.item.condition.temp
+        tempUnits = json.query.results.channel.units.temperature
+
+        $('#location').html(`The weather in ${city}, ${country} is`)
+        $('#weather-summary').html(summary)
+        $('#temperature').html(temperature + tempUnits)
     })
 })
