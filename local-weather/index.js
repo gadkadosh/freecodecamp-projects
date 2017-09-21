21754db753e77e5f21b98fe5376d5db285257f76
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-21 17:16:26 +0200

    Now getting some weather information for the current location

--- local-weather/index.js
+++ local-weather/index.js
@@ -1,29 +1,31 @@
-url = "https://query.yahooapis.com/v1/public/yql"
-query = "select * from weather.forecast where woeid = 615702"
-queryUrl = "?q=" + query.replace(/ /g, '%20').replace(/=/g, '%3D') + "&format=json"
-weatherUrl = url + queryUrl
+const makeQuery = (url, query) => 
+    url + "?q=" + query + "&format=json" + "&u=c"
 
-// console.log(weatherUrl)
-// console.log(url + "?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+const weatherQuery = (latitude, longitude) =>
+`select * from weather.forecast where woeid in (select woeid from geo.places where text="(${latitude},${longitude})")`
+
+yahooApiUrl = "https://query.yahooapis.com/v1/public/yql"
+
+const updateWeather = json => {
+    // console.log(json)
+    city = json.query.results.channel.location.city
+    country = json.query.results.channel.location.country
+    summary = json.query.results.channel.item.condition.text
+    temperature = json.query.results.channel.item.condition.temp
+    tempUnits = json.query.results.channel.units.temperature
+
+    $('#location').html(`The weather in ${city}, ${country} is`)
+    $('#weather-summary').html(summary)
+    $('#temperature').html(temperature + tempUnits)
+}
 
 $('document').ready(() => {
-    // navigator.geolocation.getCurrentPosition(pos => {
-    //     console.log("Lon:", pos.coords.longitude, "Lat", pos.coords.latitude)
-    // })
-
-    $.getJSON(weatherUrl, json => {
-        console.log('hi')
-        console.log(json.query.results.channel)
-        // console.log(JSON.stringify(json))
-
-        city = json.query.results.channel.location.city
-        country = json.query.results.channel.location.country
-        summary = json.query.results.channel.item.condition.text
-        temperature = json.query.results.channel.item.condition.temp
-        tempUnits = json.query.results.channel.units.temperature
-
-        $('#location').html(`The weather in ${city}, ${country} is`)
-        $('#weather-summary').html(summary)
-        $('#temperature').html(temperature + tempUnits)
+    navigator.geolocation.getCurrentPosition(pos => {
+        // console.log(pos)
+        latitude = pos.coords.latitude
+        longitude = pos.coords.longitude
+        // query = `select * from weather.forecast where woeid in (select woeid from geo.places where text="(${latitude},${longitude})")`
+        url = (makeQuery(yahooApiUrl, weatherQuery(latitude, longitude)))
+        $.getJSON(url, updateWeather)
     })
 })
