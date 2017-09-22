a6da0573adb68a16e2433691daf20060059e133d
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-22 19:43:01 +0200

    Local-weather: added switch for celsius/fahrenheit

--- local-weather/index.js
+++ local-weather/index.js
@@ -1,33 +1,83 @@
-const makeQuery = (url, query) => 
+"use strict"
+
+const yahooApiUrl = "https://query.yahooapis.com/v1/public/yql"
+const fccApiUrl = "https://fcc-weather-api.glitch.me/"
+let showCel = true
+
+const weatherData = {}
+
+const makeYqlQueryUrl = (url, query) => 
     url + "?q=" + encodeURIComponent(query) + "&format=json" + "&u=c"
 
-const weatherQuery = (latitude, longitude) =>
+const makeYqlQuery = (latitude, longitude) =>
     `select item.condition, location, units from weather.forecast where ` +
     `woeid in (select woeid from geo.places where ` +
     `text="(${latitude},${longitude})") and u='c'`
 
-yahooApiUrl = "https://query.yahooapis.com/v1/public/yql"
+const makeFccQueryUrl = (url, latitude, longitude) =>
+    url + `api/current?lat=${latitude}&lon=${longitude}`
 
-const updateWeather = json => {
+
+
+const updateWeatherFromYahoo = json => {
     // console.log(json)
-    city = json.query.results.channel.location.city
-    country = json.query.results.channel.location.country
-    summary = json.query.results.channel.item.condition.text
-    temperature = json.query.results.channel.item.condition.temp
-    tempUnits = json.query.results.channel.units.temperature === 'C' 
-        ? '&#8451' : '&#8457'
+    weatherData.city = json.query.results.channel.location.city
+    weatherData.country = json.query.results.channel.location.country
+    weatherData.summary = json.query.results.channel.item.condition.text
+    weatherData.temperature = json.query.results.channel.item.condition.temp
 
+    updateHtml(weatherData.city, weatherData.country, weatherData.summary, weatherData.temperature)
+}
+
+const fahToCel = deg => Math.round(deg * 9 / 5 + 32)
+
+const updateWeatherFromFcc = json => {
+    // console.log(json)
+    weatherData.city = json.name
+    weatherData.country = json.sys.country
+    weatherData.summary = json.weather.description
+    weatherData.temperature = Math.round(json.main.temp)
+
+    updateHtml(weatherData.city, weatherData.country, weatherData.summary, weatherData.temperature)
+}
+
+const updateHtml = (city, country, summary, temperature) => {
+    const tempUnits = showCel ? '&#8451' : '&#8457'
     $('#location').html(`The weather in ${city}, ${country} is`)
-    $('#weather-summary').html(summary)
+    $('#summary').html(summary)
+    temperature = showCel ? temperature : fahToCel(temperature)
     $('#temperature').html("Temperature: " + temperature +
         '<small class="align-top">' + tempUnits + '</small>')
 }
 
 $('document').ready(() => {
+    $('#unit').hide()
     navigator.geolocation.getCurrentPosition(pos => {
-        latitude = pos.coords.latitude
-        longitude = pos.coords.longitude
-        url = (makeQuery(yahooApiUrl, weatherQuery(latitude, longitude)))
-        $.getJSON(url, updateWeather)
+        const latitude = pos.coords.latitude
+        const longitude = pos.coords.longitude
+        // const url = (makeYqlQueryUrl(yahooApiUrl, makeYqlQuery(latitude, longitude)))
+        // $.getJSON(url, updateWeatherFromYahoo)
+        const url = (makeFccQueryUrl(fccApiUrl, latitude, longitude))
+        $.getJSON(url, updateWeatherFromFcc)
+        $('#unit').show()
+        $('#unit-cel').addClass('active')
+    }, error => {
+        console.log("Can't find location:", error)
+        $('#location').html("Can't find location:")
+        $('#summary').html(error.message)
+    })
+
+    $('.unit-select').on('click', e => {
+        if (!$(e.currentTarget).hasClass('active')) {
+            $('.unit-select').toggleClass('active')
+            if ($(e.currentTarget).attr('id') === 'unit-cel') {
+                showCel = true
+            } else {
+                showCel = false
+            }
+            console.log(showCel)
+            updateHtml(weatherData.city, weatherData.country,
+                weatherData.summary, weatherData.temperature)
+        }
     })
 })
