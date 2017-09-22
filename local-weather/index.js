644d20c5273a8237c6acba0951ab272f2320c129
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-22 09:58:09 +0200

    Local-weather: some basic styling

--- local-weather/index.js
+++ local-weather/index.js
@@ -1,8 +1,8 @@
 const makeQuery = (url, query) => 
-    url + "?q=" + query + "&format=json" + "&u=c"
+    url + "?q=" + encodeURIComponent(query) + "&format=json" + "&u=c"
 
 const weatherQuery = (latitude, longitude) =>
-`select * from weather.forecast where woeid in (select woeid from geo.places where text="(${latitude},${longitude})")`
+`select item.condition, location, units from weather.forecast where woeid in (select woeid from geo.places where text="(${latitude},${longitude})") and u='c'`
 
 yahooApiUrl = "https://query.yahooapis.com/v1/public/yql"
 
@@ -12,19 +12,17 @@ const updateWeather = json => {
     country = json.query.results.channel.location.country
     summary = json.query.results.channel.item.condition.text
     temperature = json.query.results.channel.item.condition.temp
-    tempUnits = json.query.results.channel.units.temperature
+    tempUnits = json.query.results.channel.units.temperature === 'C' ? '&#8451' : '&#8457'
 
     $('#location').html(`The weather in ${city}, ${country} is`)
     $('#weather-summary').html(summary)
-    $('#temperature').html(temperature + tempUnits)
+    $('#temperature').html("Temperature: " + temperature + '<small class="align-top">' + tempUnits + '</small>')
 }
 
 $('document').ready(() => {
     navigator.geolocation.getCurrentPosition(pos => {
-        // console.log(pos)
         latitude = pos.coords.latitude
         longitude = pos.coords.longitude
-        // query = `select * from weather.forecast where woeid in (select woeid from geo.places where text="(${latitude},${longitude})")`
         url = (makeQuery(yahooApiUrl, weatherQuery(latitude, longitude)))
         $.getJSON(url, updateWeather)
     })
