0cdd273188886d89060072d3c607dabf906578c0
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-26 01:01:54 +0200

    Wikipedia: Now with Opensearch, better search, more results.

--- wikipedia-viewer/index.js
+++ wikipedia-viewer/index.js
@@ -1,7 +1,14 @@
 "use strict"
 
 const endpoint = "https://en.wikipedia.org/w/api.php"
-const queryUrl = `${endpoint}?action=query&titles=Mozart&prop=info&inprop=url&format=json&origin=*`
+// const queryUrl = `${endpoint}?action=query&titles=Mozart&prop=info&inprop=url&format=json&origin=*`
+// const queryUrl = `${endpoint}?action=opensearch&search=Mozart&prop=info&inprop=url&format=json&origin=*`
+
+const showSearchBar = () => {
+    $('#search-here').fadeOut(500, () => {
+        $('')
+    })
+}
 
 const initiateSearch = keyword => {
     keyword = typeof keyword === 'string' ? keyword : $('#input-search').val()
@@ -10,55 +17,50 @@ const initiateSearch = keyword => {
         url: endpoint,
         data: {
             origin: "*",
-            action: "query",
+            action: "opensearch",
             format: "json",
-            origin: "*",
-            prop: "info",
-            inprop: "url",
-            titles: keyword
+            search: keyword
         },
         success: showResults
     })
 }
 
 const showResults = json => {
-    console.log(json.query.pages)
+    console.log(json)
 
-    const keys = Object.keys(json.query.pages)
-    const allResults = keys.map((key, i) => json.query.pages[key])
-    // console.log(allResults)
+    const allResults = json[1].map(x => ({ title: x }))
+    json[2].forEach((x, i) => {
+        Object.assign(allResults[i], { description: x.slice(0, 140) + '...' })
+    })
+    json[3].forEach((x, i) => {
+        Object.assign(allResults[i], { url: x })
+    })
 
-    const resultsHtml = allResults.map(entry => 
-        `<li><a href="${entry.fullurl}">${entry.title}</a></li>`)
+    const resultsHtml = allResults.length !== 0 
+        ? allResults.map(entry => 
+        `<li class="list-unstyled my-4">
+            <a href="${entry.url}">
+                <p class="font-weight-bold mb-0">${entry.title}</p>
+                <p class="font-smaller">${entry.description}</p>
+            </a></li>`)
+        : `<p>No results found! Please try again</p>`
 
-    $('#search-results').html(resultsHtml.join(''))
+    $('#search-results').html(typeof resultsHtml === 'array'
+        ? resultsHtml.join('')
+        : resultsHtml)
 }
 
 $('document').ready(() => {
-    // $.getJSON(queryUrl, json => {
-    //     console.log('hi', json)
-    // })
-
-    initiateSearch('andromeda')
-    // $.ajax({
-    //     url: endpoint,
-    //     data: {
-    //         origin: "*",
-    //         action: "query",
-    //         format: "json",
-    //         origin: "*",
-    //         prop: "info",
-    //         inprop: "url",
-    //         titles: "Mozart"
-    //     },
-    //     success: showResults
-    // })
+    $('#search-here').on('click', showSearchBar)
 
     $('#btn-search').on('click', initiateSearch)
+
     $('#input-search').on('keydown', e => {
         if (e.keyCode == 13) {
             initiateSearch()
             e.preventDefault()
         } 
       });
+
+      initiateSearch('Mozart')
 });
