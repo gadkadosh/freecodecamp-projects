b0217842fe690732fbf9ee237124ad3fe215a0e9
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-26 15:46:01 +0200

    Some progress on styling, nearly satisfied

--- wikipedia-viewer/index.js
+++ wikipedia-viewer/index.js
@@ -1,12 +1,10 @@
 "use strict"
 
 const endpoint = "https://en.wikipedia.org/w/api.php"
-// const queryUrl = `${endpoint}?action=query&titles=Mozart&prop=info&inprop=url&format=json&origin=*`
-// const queryUrl = `${endpoint}?action=opensearch&search=Mozart&prop=info&inprop=url&format=json&origin=*`
 
-const showSearchBar = () => {
-    $('#search-here').fadeOut(500, () => {
-        $('')
+const showSearchForm = () => {
+    $('#search-here, #random-article').fadeOut(300, () => {
+        $('#search-form').fadeIn(400)
     })
 }
 
@@ -29,31 +27,41 @@ const showResults = json => {
     console.log(json)
 
     const allResults = json[1].map(x => ({ title: x }))
-    json[2].forEach((x, i) => {
-        Object.assign(allResults[i], { description: x.slice(0, 140) + '...' })
-    })
-    json[3].forEach((x, i) => {
-        Object.assign(allResults[i], { url: x })
-    })
+        .map((x, i) => Object.assign(x, { description: json[2][i] }))
+        .map((x, i) => Object.assign(x, { url: json[3][i] }))
+    
+    // console.log(allResults)
+        
+    const resultsHtml = allResults.length !== 0 ?
+        allResults.map(entry => 
+            `<li class="my-4">
+                <a href="${entry.url}">
+                    <p class="font-weight-bold mb-0">${entry.title}</p>
+                    <p class="font-smaller">${entry.description ? entry.description.slice(0, 140) + '...' : ''}</p>
+                </a></li>`) :
+        `<p>No results found! Please try again</p>`
+    
+    // console.log(resultsHtml)
 
-    const resultsHtml = allResults.length !== 0 
-        ? allResults.map(entry => 
-        `<li class="list-unstyled my-4">
-            <a href="${entry.url}">
-                <p class="font-weight-bold mb-0">${entry.title}</p>
-                <p class="font-smaller">${entry.description}</p>
-            </a></li>`)
-        : `<p>No results found! Please try again</p>`
-
-    $('#search-results').html(typeof resultsHtml === 'array'
-        ? resultsHtml.join('')
-        : resultsHtml)
+    $('#search-window').fadeIn(400)
+    $('#search-results').html(resultsHtml.join(''))
 }
 
+const resetState = () => {
+    $('#search-form').fadeOut(200, () => 
+        $('#search-window').fadeOut(200, () => 
+            $('#random-article-btn').fadeOut(200, () => $('#search-here, #random-article').fadeIn(500))))
+    $('#input-search').val('')
+}
+    
 $('document').ready(() => {
-    $('#search-here').on('click', showSearchBar)
+    $('#search-form').hide()
+    $('#search-window').hide()
+    $('#random-article-btn').hide()
 
+    $('#search-here').on('click', showSearchForm)
     $('#btn-search').on('click', initiateSearch)
+    $('#btn-reset').on('click', resetState)
 
     $('#input-search').on('keydown', e => {
         if (e.keyCode == 13) {
@@ -62,5 +70,5 @@ $('document').ready(() => {
         } 
       });
 
-      initiateSearch('Mozart')
+    //   initiateSearch('Mozart')
 });
