b37a94ff31c79c94e8dffb6c58c97412bce4f7ad
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-28 21:34:42 +0200

    TwitchTV: now fetching data across all three endpoints and aggregate together

--- twitchtv/index.js
+++ twitchtv/index.js
@@ -29,33 +29,65 @@ const fillItemChannel = entry => {
 
 const fillItemUser = entry => {
     // console.log(entry)
-    return `<li class="row border border-top-0">
+    const name = entry.display_name
+    const logo = entry.logo ? entry.logo : ''
+    const bio = entry.bio ?
+        entry.bio.split(' ').slice(0, 7).join(' ') + '...' :
+        'Status unavailable'
+
+    return (
+        `<li class="row border border-top-0">
             <div class="col">
-                <img src="${entry.logo}" alt="${entry.display_name}" width="64px">
-                ${entry.display_name}</div>
-            <div class="col">Status</div>
+                <img src="${logo}" alt="${name}" width="64px">
+                ${name}</div>
+            <div class="col">Status<br>${bio}</div>
             <div class="col"></div>
         </li>`
+    )
 }
 
 const updateList = (listElem, data) => {
     console.log(data)
-    console.log(fillItemUser(data[0]))
     const list = data.map(fillItemUser)
-    // console.log(list)
     listElem.append(list)
 }
 
 $(document).ready(() => {
     $('#search-bar').on('keydown', e => e.preventDefault())
 
-    // const streamUrls = channels.map(channel => urlEndpoint + 'streams/' + channel)
-    const streamUrls = channels.map(channel => urlEndpoint + 'users/' + channel)
-    const allData = []
-    const requests = streamUrls.map(url => $.getJSON(url)
-        .then(data => allData.push(data)))
+    const userUrl = channels.map(channel => urlEndpoint + 'users/' + channel)
+    const usersData = []
+    let requests = userUrl.map((url, index) => $.getJSON(url)
+        .then(data => usersData[index] = {
+            name: data.display_name,
+            logo: data.logo,
+            bio: data.bio
+        }))
+    
+    const streamUrls = channels.map(channel => urlEndpoint + 'streams/' + channel)
+    const streamsData = []
+    requests = requests.concat(streamUrls.map((url, index) => $.getJSON(url)
+        .then(data => 
+            data.stream ? 
+                streamsData[index] = {live: true, stream: data.stream} :
+                streamsData[index] = {live: false}
+        )))
+
+    const channelsData = []
+    const channelUrls = channels.map(channel => urlEndpoint + 'channels/' + channel)
+    requests = requests.concat(channelUrls.map((url, index) => $.getJSON(url)
+        .then(data => channelsData[index] = {
+            url: data.url,
+            followers: data.followers,
+            status: data.status
+        })))
+
 
     $.when.apply($, requests).then(() => {
-        updateList($('#channels-list'), allData)
+        console.log(usersData.map((x, i) => Object.assign(x, streamsData[i], channelsData[i])))
+        
+        // updateList($('#channels-list'), allData)
     })
+
+    
 })
