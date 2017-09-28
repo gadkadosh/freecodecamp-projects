8d4ee18334dace69cd7850fee44ec38cd973e58d
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-28 21:51:50 +0200

    TwitchTV: better code, better functionality

--- twitchtv/index.js
+++ twitchtv/index.js
@@ -1,54 +1,36 @@
 'use static'
 
 const urlEndpoint = 'https://wind-bow.glitch.me/twitch-api/'
-const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+// const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
-const fillItemChannel = entry => {
-    let item
-    if (entry.stream === null) {
-        item = 
-        `<li class="row border border-top-0">
-            <div class="col">${name}</div>
-            <div class="col">Channel offline</div>
-            <div class="col"></div>
-        </li>`
-    } else {
-        item = 
-        `<li class="row border border-top-0">
-            <div class="col">
-                <img src="${entry.stream.channel.logo}" width="64px">${entry.stream.channel.display_name}</div>
-            <div class="col">
-                <a href="${entry.stream.channel.url}">
-                    Channel live, ${entry.stream.viewers} viewers</a>
-                    </div>
-            <div class="col">${entry.stream.channel.status}</div>
-        </li>`
-    }
-    return item
-}
-
-const fillItemUser = entry => {
+const fillItem = entry => {
     // console.log(entry)
-    const name = entry.display_name
+    const name = entry.name
     const logo = entry.logo ? entry.logo : ''
     const bio = entry.bio ?
         entry.bio.split(' ').slice(0, 7).join(' ') + '...' :
-        'Status unavailable'
+        ''
+    const status = entry.live ? 
+        `Channel live, ${entry.stream.viewers} viewers` :
+        'Channel offline'
+    const followers = entry.followers
 
     return (
-        `<li class="row border border-top-0">
-            <div class="col">
-                <img src="${logo}" alt="${name}" width="64px">
-                ${name}</div>
-            <div class="col">Status<br>${bio}</div>
-            <div class="col"></div>
+        `<li class="row border border-top-0 py-3">
+            <div class="col-4 text-larger"><a href="${entry.url}">
+                <img src="${logo}" alt="${name}" width="64px" class="mr-3">
+                ${name}</a></div>
+            <div class="col-4"><p class="font-weight-bold">${status}</p>
+                <p>${bio}</p></div>
+            <div class="col-4"></div>
         </li>`
     )
 }
 
 const updateList = (listElem, data) => {
     console.log(data)
-    const list = data.map(fillItemUser)
+    const list = data.map(fillItem)
     listElem.append(list)
 }
 
@@ -84,9 +66,8 @@ $(document).ready(() => {
 
 
     $.when.apply($, requests).then(() => {
-        console.log(usersData.map((x, i) => Object.assign(x, streamsData[i], channelsData[i])))
-        
-        // updateList($('#channels-list'), allData)
+        const allData = usersData.map((x, i) => Object.assign(x, streamsData[i], channelsData[i]))
+        updateList($('#channels-list'), allData)
     })
 
     
