77144d260851b5f992f4ccd282448c5c5f06e74f
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-28 19:29:19 +0200

    TwitchTV: cleaner code, but still looking for the right information to show

--- twitchtv/index.js
+++ twitchtv/index.js
@@ -3,14 +3,9 @@
 const urlEndpoint = 'https://wind-bow.glitch.me/twitch-api/'
 const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
-const updateChannel = (name, data) => {
-    console.log(data)
-    // const item = data.stream === null ?
-    //     `<li>Channel offline</li>` :
-    //     `<li>${data.stream._id}</li>`
-
-    let item = ''
-    if (data.stream === null) {
+const fillItemChannel = entry => {
+    let item
+    if (entry.stream === null) {
         item = 
         `<li class="row border border-top-0">
             <div class="col">${name}</div>
@@ -21,21 +16,46 @@ const updateChannel = (name, data) => {
         item = 
         `<li class="row border border-top-0">
             <div class="col">
-                <img src="${data.stream.channel.logo}" width="64px">${data.stream.channel.display_name}</div>
+                <img src="${entry.stream.channel.logo}" width="64px">${entry.stream.channel.display_name}</div>
             <div class="col">
-                <a href="${data.stream.channel.url}">
-                    Channel live, ${data.stream.viewers} viewers</a>
+                <a href="${entry.stream.channel.url}">
+                    Channel live, ${entry.stream.viewers} viewers</a>
                     </div>
-            <div class="col">${data.stream.channel.status}</div>
+            <div class="col">${entry.stream.channel.status}</div>
         </li>`
     }
-    
-    // console.log(item)
-    $('#channels-list').append(item)
+    return item
+}
+
+const fillItemUser = entry => {
+    // console.log(entry)
+    return `<li class="row border border-top-0">
+            <div class="col">
+                <img src="${entry.logo}" alt="${entry.display_name}" width="64px">
+                ${entry.display_name}</div>
+            <div class="col">Status</div>
+            <div class="col"></div>
+        </li>`
+}
+
+const updateList = (listElem, data) => {
+    console.log(data)
+    console.log(fillItemUser(data[0]))
+    const list = data.map(fillItemUser)
+    // console.log(list)
+    listElem.append(list)
 }
 
 $(document).ready(() => {
-    channels.map(channel => urlEndpoint + 'streams/' + channel)
-    .forEach((url, i) =>
-        $.getJSON(url).then(data => updateChannel(channels[i], data)))
+    $('#search-bar').on('keydown', e => e.preventDefault())
+
+    // const streamUrls = channels.map(channel => urlEndpoint + 'streams/' + channel)
+    const streamUrls = channels.map(channel => urlEndpoint + 'users/' + channel)
+    const allData = []
+    const requests = streamUrls.map(url => $.getJSON(url)
+        .then(data => allData.push(data)))
+
+    $.when.apply($, requests).then(() => {
+        updateList($('#channels-list'), allData)
+    })
 })
