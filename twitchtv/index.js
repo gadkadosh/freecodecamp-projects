95766d226654ddfcfe44d72b33218ed3f122cf5b
Gad Kadosh <gad.kadosh@gmail.com>
2017-09-29 10:06:32 +0200

    TwitchTV: styling

--- twitchtv/index.js
+++ twitchtv/index.js
@@ -11,18 +11,31 @@ const fillItem = entry => {
     const bio = entry.bio ?
         entry.bio.split(' ').slice(0, 12).join(' ') + '...' :
         ''
-    const status = entry.live ? 
-        `Channel live, ${entry.stream.viewers} viewers` :
-        'Channel offline'
     const followers = entry.followers
+    const status = entry.live ? 
+        `<a href="${entry.url}" target="_blank">Channel live</a>` :
+        'Channel offline'
+    const viewers = entry.live ?
+        `<p class="mt-3">${entry.stream.viewers} viewers</p>` :
+        ''
+    const preview = entry.live ?
+        `<img src="${entry.stream.preview.medium}" class="img-fluid">` :
+        ''
 
     return (
         `<li class="row border-bottom py-3 hover-bg-light">
-            <div class="col-12 col-sm-4  my-3 text-center text-larger"><a href="${entry.url}">
-                <img src="${logo}" alt="${name}" width="64px" class="mx-auto mb-3 d-block">
+            <div class="col-12 col-sm-3 align-self-center my-3 text-center text-larger text-break-word">
+                <a href="${entry.url}" target="_blank">
+                    <img src="${logo}" alt="${name}" width="64px" class="mx-auto mb-3 d-block">
                 ${name}</a></div>
-            <div class="col-12 col-sm-8 my-3 text-center text-sm-left"><p class="font-weight-bold">${status}</p>
-                <p>${bio}</p></div>
+            <div class="col-12 col-sm-5 align-self-center my-3 text-center">
+                <p class="text-break-word">${bio}</p>
+                <p class="font-weight-bold">${followers} followers</p></div>
+            <div class="col-12 col-sm-4 align-self-center text-center">
+                <p class="font-weight-bold">${status}</p>
+                ${preview}
+                ${viewers}
+            </div>
         </li>`
     )
 }
@@ -34,7 +47,13 @@ const updateList = (listElem, data) => {
 }
 
 $(document).ready(() => {
-    $('#search-bar').on('keydown', e => e.preventDefault())
+    $('#search-bar').on('keydown', e => {
+        if(e.keyCode === 13) {
+            e.preventDefault()
+        } else if(e.keyCode === 27) {
+            $('#search-bar').val('')
+        }
+    })
 
     const userUrl = channels.map(channel => urlEndpoint + 'users/' + channel)
     const usersData = []
