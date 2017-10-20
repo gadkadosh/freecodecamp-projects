5574afb33276727b5d6895b1825dde249c3171d6
Gad Kadosh <gad.kadosh@gmail.com>
2017-10-20 22:59:50 +0200

    JS-Calc: refactored into object, chaining now works

--- js-calc/index.js
+++ js-calc/index.js
@@ -1,104 +1,125 @@
 'use static'
 
-let currentNumber = 0
-let numbers = []
-let operation
-
-// const currentNumber = () => {
-//     return numbers[numbers.length - 1] || 0
-// }
-
-/* Functions */
-const updateDisplay = (num = currentNumber) => {
-    strNumber = String(num)
-    if(strNumber.includes('.')) {
-        display.innerText = String(strNumber)
-        return
-    }
-    display.innerText = String(strNumber)
-    // displayElement.innerText = String(strNumber) + '.'
+const operationsKey = {
+    add: {
+        symbol: '+',
+        fn: (a, b) => Number(a) + Number(b)
+    },
+    subtract: {
+        symbol: '-',
+        fn: (a, b) => Number(a) - Number(b)
+    },
+    multiply: {
+        symbol: 'x',
+        fn: (a, b) => Number(a) * Number(b)
+    },
+    divide: {
+        symbol: '/',
+        fn: (a, b) => Number(a) / Number(b)
+    },
 }
 
-const resetCalc = () => {
-    currentNumber = 0
-    numbers = []
-    operation = null
-}
+const calculator = {
 
-const digitButtonHandler = e => {
-    const digit = e.currentTarget.innerText
-    // console.log(digit)
-    addDigit(digit)
-    updateDisplay()
-}
+    displayElem: document.getElementById('display'),
+    operationDisplayElem: document.getElementById('operation-display'),
+    currentNumber: 0,
+    lastInput: '',
+    numbers: [],
+    operations: [],
 
-const addDigit = (digit) => {
-    if (String(currentNumber).length >= 8) return
-    if (currentNumber === 0) {
-        currentNumber = digit
-        console.log(currentNumber)
-        return
-    }
-    console.log(String(currentNumber) + String(digit))
-    currentNumber = String(currentNumber) + String(digit)
-    console.log(currentNumber)
-}
+    reset: function() {
+        this.currentNumber = 0
+        this.numbers = []
+        this.operations = []
+        this.updateDisplay()
+    },
 
-const saveNumber = (number) => {
-    numbers = numbers.concat([currentNumber])
-}
+    addDigit: function(digit) {
+        if (this.lastInput === 'equal') this.reset()
+        if (String(this.currentNumber).length >= 8) return
+        if (this.currentNumber === 0) {
+            this.currentNumber = digit
+        } else {
+        console.log(digit)
+            this.currentNumber = String(this.currentNumber) + String(digit)
+            // this.currentNumber = String(this.currentNumber) + String(digit)
+        }
+        this.updateDisplay()
+        this.lastInput = 'digit'
+    },
 
-// Operations
-const add = (a, b) => Number(a) + Number(b)
-const subtract = (a, b) => Number(a) - Number(b)
-const multiply = (a, b) => Number(a) * Number(b)
-const divide = (a, b) => Number(a) / Number(b)
+    invertNumber: function() {
+        this.currentNumber *= -1
+        this.updateDisplay()
+    },
 
-const selectOperation = strOperation => {
-    switch (strOperation) {
-        case 'add':
-        return add
-        case 'subtract':
-        return subtract
-        case 'multiply':
-        return multiply
-        case 'divide':
-        return divide
+    saveNumber: function(num) {
+        num = num || this.currentNumber
+        this.numbers.push(num)
+        this.currentNumber = 0
+        // don't update display, when clicking an operation button the currentNumber stays displayed
+    },
+
+    setOperation: function (strOperation) {
+        if (this.lastInput === 'digit') {
+            this.saveNumber()
+        } else if (this.lastInput === 'operation') {
+            this.operations.pop()
+        }
+        this.operations.push(operationsKey[strOperation].fn)
+        this.lastInput = 'operation'
+    },
+
+    calculate: function() {
+        const answer = this.numbers.reduce((acc, x, i, all) =>
+            this.operations[i - 1](acc, x))
+        this.updateDisplay(answer)
+        return answer
+    },
+
+    updateDisplay: function(num) {
+        num = num || this.currentNumber
+        this.displayElem.innerText = String(num)
+        const lastOperation = this.operations[this.operations.length - 1]
+        const symbol = operationsKey[lastOperation]
+        this.operationDisplayElem.innerText = symbol || ''
     }
 }
 
 // Digit buttons: add digit to current number
+const digitBtnHandler = e => {
+    const digit = e.currentTarget.innerText
+    calculator.addDigit(digit)
+}
+
 const digitBtnArr = Array.from(document.getElementsByClassName('btn-digit'))
-digitBtnArr.forEach(btn => btn.addEventListener('click', digitButtonHandler))
+digitBtnArr.forEach(btn => btn.addEventListener('click', digitBtnHandler))
 
 const acButton = document.getElementById('ac-btn')
-acButton.addEventListener('click', e => {
-    resetCalc()
-    updateDisplay()
-})
+acButton.addEventListener('click', e => { calculator.reset() })
 
-const plusMinusBtn = document.getElementById('plus-minus-btn')
-plusMinusBtn.addEventListener('click', e => {
-    currentNumber *= -1
-    updateDisplay()
-})
+const invertBtn = document.getElementById('invert-btn')
+invertBtn.addEventListener('click', e => { calculator.invertNumber() })
+
+const operationBtnHandler = e => {
+    calculator.setOperation(e.currentTarget.dataset.operation)
+}
 
 const operatorBtnArr = Array.from(document.getElementsByClassName('btn-operator'))
-operatorBtnArr.forEach(btn => btn.addEventListener('click', e => {
-    saveNumber(currentNumber)
-    if (e.currentTarget.dataset.operation) {
-        operation = selectOperation(e.currentTarget.dataset.operation)
-        currentNumber = 0
-        console.log(e.currentTarget.dataset.operation, operation)
-    } else if (numbers.length === 2 && operation) {
-        // This would be the equal button case
-        console.log(numbers, operation)
-        const result = operation(numbers[0], numbers[1])
-        console.log(result)
-        updateDisplay(result)
-        resetCalc()
-    }
-}))
+operatorBtnArr.forEach(btn => btn.addEventListener('click', operationBtnHandler))
 
-const display = document.getElementById('display')
-updateDisplay()
+const eqBtnHandler = e => {
+    if (calculator.lastInput === 'digit') {
+        calculator.saveNumber()
+    }
+    console.log(calculator.numbers, calculator.operations)
+    if (calculator.numbers.length > 1 && calculator.operations.length > 0) {
+        calculator.calculate()
+    }
+    calculator.lastInput = 'equal'
+}
+
+document.getElementById('btn-eq').addEventListener('click', eqBtnHandler)
+
+calculator.updateDisplay()
